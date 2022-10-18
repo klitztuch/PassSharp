7621037c739a80a34ddf08a9abbcea5ac3dbc863
Markus Rothlübbers <markus@mrlb.space>
2022-10-18 17:55:32 +0200

    Fixes PasswordTest.cs with GpgService

--- PassSharp.Lib.Test/PasswordTest.cs
+++ PassSharp.Lib.Test/PasswordTest.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Security;
+using System.Linq;
+using Moq;
+using PassSharp.Lib.Service.Abstraction;
 using Xunit;
 
 namespace PassSharp.Lib.Test;
@@ -12,22 +13,31 @@ public class PasswordTest : IDisposable
     {
     }
 
-    [Fact(Skip = "Draft")]
+    [Fact]
     public async void ShowTest()
     {
         // arrange
         const string passwordPath = "testpath";
-        const string expectedPasswordString = "test";
-        var expectedPasswordSecureString = new NetworkCredential("", expectedPasswordString).SecurePassword;
-        IEnumerable<SecureString> expectedPassword = new List<SecureString>
+        const string expectedPassword = "test1";
+        const string expectedInformation = "login: test";
+        IEnumerable<string> expectedResult = new List<string>
         {
-            expectedPasswordSecureString
+            expectedPassword,
+            expectedInformation
         };
+        var gpgServiceMock = new Mock<IGpgService>();
+        gpgServiceMock.Setup(service => service.Decrypt(It.IsAny<string>()))
+            .ReturnsAsync((string path) => expectedResult.ToArray());
 
-        var pass = new Password(passwordPath);
+
+        var pass = new Password(gpgServiceMock.Object, passwordPath);
         // act
-        var result = await pass.Show();
+        var actualResult = await pass.Show();
         // assert
-        Assert.IsAssignableFrom<IEnumerable<SecureString>>(result);
+        gpgServiceMock.Verify(service => service.Decrypt(It.IsAny<string>()), Times.Once);
+        Assert.IsAssignableFrom<IEnumerable<string>>(actualResult);
+        Assert.Equal(expectedResult.Count(), actualResult.Count());
+        Assert.Collection(actualResult, actualPassword => Assert.Equal(expectedPassword, actualPassword),
+            actualInformation => Assert.Equal(expectedInformation, actualInformation));
     }
 }
